e0929b883e6a42f07450c98531b9c95c21918e07
Taylor Bolas <gyroninja@gmail.com>
2014-07-25 23:58:35 -0700

    Add basic interface for sewer parts

---
+++ src/main/java/net/gyroninja/Sewers/SewerPart.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.gyroninja.Sewers;
+
+import java.util.Random;
+import org.bukkit.Location;
+
+/**
+ *
+ * @author gyroninja
+ */
+public interface SewerPart {
+
+	public abstract void generate(Location start, int length, Random gen);
+}
