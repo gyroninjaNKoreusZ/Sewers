85a4dc59231797d7a092a7a904932bfb03891f0f
Taylor Bolas <gyroninja@gmail.com>
2014-07-25 23:58:55 -0700

    Initial class for generating sewers

---
+++ src/main/java/net/gyroninja/Sewers/SewerGenerator.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.gyroninja.Sewers;
+
+import java.util.Random;
+import org.bukkit.Location;
+
+/**
+ *
+ * @author gyroninja
+ */
+public class SewerGenerator implements SewerPart {
+
+	public void generate(Location start, int length, Random gen) {
+
+		
+	}
+}
