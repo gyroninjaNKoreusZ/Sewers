653162df0ba7bfc8bb2408147f13fa882dd47b68
Taylor Bolas <gyroninja@gmail.com>
2014-07-26 20:36:37 -0700

    Fixed spelling of entrance

---
+++ src/main/java/net/gyroninja/Sewers/SewerEntrance.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.gyroninja.Sewers;
+
+import java.util.Random;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+
+/**
+ *
+ * @author gyroninja
+ */
+public class SewerEntrance implements SewerPart {
+
+	public Block generate(Block start, int length, BlockFace bf, Random gen) {
+
+		return null;
+	}
+}
