4153ac3229257e01f49cb110c9d23c1133e75205
Taylor Bolas <gyroninja@gmail.com>
2014-07-26 20:58:15 -0700

    Added basic tunnel

---
+++ src/main/java/net/gyroninja/Sewers/SewerTunnel.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.gyroninja.Sewers;
+
+import java.util.Random;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+
+/**
+ *
+ * @author gyroninja
+ */
+public class SewerTunnel implements SewerPart {
+
+	public Block generate(Block start, int length, BlockFace bf, Random gen) {
+
+		
+
+		return null;
+	}
+}
